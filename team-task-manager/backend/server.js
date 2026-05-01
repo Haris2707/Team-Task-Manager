3254554fcfeca03ac58eb702cd0983b6c8c4b2df
Haris Ali <151764409+Haris2707@users.noreply.github.com>
2026-05-01 20:18:11 +0530

    Update server.js

--- team-task-manager/backend/server.js
+++ team-task-manager/backend/server.js
@@ -7,7 +7,10 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: [
+    "http://localhost:3000",
+    process.env.FRONTEND_URL
+  ],
   credentials: true
 }));
 app.use(express.json());
